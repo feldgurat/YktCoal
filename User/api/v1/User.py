72d6cd9f881dc1a80d580685cb807a3d185beb3d
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-03-17 12:46:03 +0900

    update api v1

--- User/api/v1/User.py
+++ User/api/v1/User.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from api.routes import API_V1_PREFIX, USERS
 from api.v1.dependencies import CurrentPersonDep, get_current_person
 from data.Database import SessionDep
-from data.schemas.User import UserCreateWithPerson, UserRead
+from data.schemas.Common import DeleteResponse
+from data.schemas.User import UserCreateWithPerson, UserRead, UserUpdate
 from services.UserService import UserServiceDep
 
 
@@ -58,3 +59,23 @@ async def get_my_profile(userService: UserServiceDep, current_person: CurrentPer
             detail="User not found",
         )
     return user
+
+@router.patch("", response_model=UserRead)
+async def edit_user(id: UUID, user: UserUpdate, userService: UserServiceDep):
+    user = await userService.update(id, user)
+    await userService.session.commit()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    return user
+
+@router.delete("", response_model=DeleteResponse)
+async def delete_user(id: UUID, userService: UserServiceDep):
+    succ = await userService.delete(id)
+    if succ:
+        await userService.session.commit()
+        return DeleteResponse(success=succ, status="User deleted. Person already exist.")
+    else:
+        return DeleteResponse(success=succ, status="Fail. User not exist")
