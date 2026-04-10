1c7612f9b495734b311349ae1052013ec524817e
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-04-11 06:37:47 +0900

    da hospade

--- User/data/entities/User.py
+++ User/data/entities/User.py
@@ -10,7 +10,7 @@ class User(SQLModel, table=True):
     __tablename__ = "users"
 
     id: uuid.UUID = Field(
-        default_factory=uuid.uuid4(),
+        default_factory=uuid.uuid4,
         primary_key=True
     )
     name: str = Field(max_length=255)
