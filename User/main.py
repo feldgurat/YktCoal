da92a155f216b0aea896199fbcd046b4d3b68868
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-04-24 12:46:27 +0900

    update cors

--- User/main.py
+++ User/main.py
@@ -48,7 +48,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # на проде замените на конкретные домены
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
