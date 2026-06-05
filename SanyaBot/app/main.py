dc5824c081b78ce7b21b94d63c1db89f613193a3
Vlad <vladislav.v.skryabin@mail.ru>
2026-06-05 19:37:19 +0900

    фикс main

--- SanyaBot/app/main.py
+++ SanyaBot/app/main.py
@@ -63,5 +63,5 @@ async def main() -> None:
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except KeyboardInterrupt, SystemExit:
+    except (KeyboardInterrupt, SystemExit):
         pass
